d3c56b0fb94e025a3816d980f5e867cfabf2b75e
Tim Sander <tisande@microsoft.com>
2021-08-25 11:25:20 -0700

    adding starting code

---
+++ SampleDataGenerator/Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.Azure.Cosmos;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.Json;
+
+namespace SampleDataGenerator
+{
+    class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            DataIngestionTool ingest = new DataIngestionTool();
+            await ingest.RunBenchmark();
+        }
+    }
+    class DataIngestionTool
+    {
+        public CosmosClient client;
+        public Container container;
+        public string databaseId;
+        public string containerId;
+        public string partitionKeyPath;
+
+        public async Task RunBenchmark()
+        {
+            bool exit = false;
+
+            while (exit == false)
+            {
+                Console.Clear();
+                Console.WriteLine($"Cosmos DB data generator");
+                Console.WriteLine($"-----------------------------------------------------------");
+                Console.WriteLine($"[1]   Ingest data");
+                Console.WriteLine($"[2]   Exit");
+
+                ConsoleKeyInfo result = Console.ReadKey(true);
+
+                if (result.KeyChar == '1')
+                {
+                    Console.Clear();
+                    await InitialIngest();
+                }
+                else if (result.KeyChar == '2')
+                {
+                    exit = true;
+                }
+            }
+        }
+
+        public async Task InitialIngest()
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                       .AddJsonFile("AppSettings.json")
+                       .Build();
+
+            this.client = new CosmosClient(configuration["accountEndpoint"], configuration["accountKey"], new CosmosClientOptions { ConnectionMode = ConnectionMode.Direct, AllowBulkExecution = true });
+            this.containerId = configuration["containerId"];
+            this.databaseId = configuration["databaseId"];
+            this.partitionKeyPath = configuration["partitionKeyPath"];
+
+            //Verify the benchmark is setup
+            await InitializeBenchmark();
+
+            int numberOfItems = 10000;
+            int totalItemsUploadedSoFar = 0;
+
+            Console.WriteLine($"\n Initial Data Ingest \nPress any key to continue\n...");
+            Console.ReadKey(true);
+
+            while (true)
+            {
+                //Customers to insert
+                List<SampleCustomer> customers = SampleCustomer.GenerateManyCustomers(configuration["partitionKeyPath"], numberOfItems);
+
+                foreach (SampleCustomer customer in customers)
+                {
+                    ItemResponse<SampleCustomer> response = await container.CreateItemAsync<SampleCustomer>(customer);
+                    totalItemsUploadedSoFar++;
+                    if (totalItemsUploadedSoFar % 10 == 0)
+                    {
+                        Console.WriteLine($"Bulk inserted {totalItemsUploadedSoFar} items into a Cosmos container");
+                    }
+                }
+            }
+        }
+
+        public async Task InitializeBenchmark()
+        {
+            try
+            {
+                ItemRequestOptions itemRequestOptions = new ItemRequestOptions { ConsistencyLevel = ConsistencyLevel.Eventual };
+                QueryRequestOptions queryRequestOptions = new QueryRequestOptions { ConsistencyLevel = ConsistencyLevel.Eventual };
+
+                this.container = this.client.GetContainer(this.databaseId, this.containerId);
+                await this.container.ReadContainerAsync();  //ReadContainer to see if it is created
+            }
+
+            catch
+            {
+                // If container has not been created, create it
+                Database database = await this.client.CreateDatabaseIfNotExistsAsync(this.databaseId);
+                Container container = await database.CreateContainerIfNotExistsAsync(this.containerId, partitionKeyPath, 6000);
+                this.container = container;
+
+                // Ingest some data
+                await InitialIngest();
+            }
+        }
+    }
+}
---
+++ SampleDataGenerator/SampleCustomer.cs
@@ -0,0 +1,126 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SampleDataGenerator
+{
+    public class SampleCustomer
+    {
+        [JsonProperty(PropertyName = "myPartitionKey")]
+        public string MyPartitionKey { get; set; }
+
+        [JsonProperty(PropertyName = "id")]
+        public string Id { get; set; }
+
+        [JsonProperty(PropertyName = "name")]
+        public string Name { get; set; }
+
+        [JsonProperty(PropertyName = "city")]
+        public string City { get; set; }
+
+        [JsonProperty(PropertyName = "postalcode")]
+        public string PostalCode { get; set; }
+
+        [JsonProperty(PropertyName = "region")]
+        public string Region { get; set; }
+
+        [JsonProperty(PropertyName = "userDefinedId")]
+        public int UserDefinedId { get; set; }
+
+        [JsonProperty(PropertyName = "wordsArray")]
+        public string[] WordsArray { get; set; }
+
+        [JsonProperty(PropertyName = "tags")]
+        public Tags Tags { get; set; }
+
+        [JsonProperty(PropertyName = "recipientList")]
+        public ArrayList RecipientList { get; set; }
+
+        public static List<SampleCustomer> GenerateManyCustomers(string partitionKeyValue, int number)
+        {
+            Tags tags = new Tags();
+            ArrayList RecipientList = new ArrayList();
+            int numberOfAddressInfo = 0;
+
+            //Generate fake customer data.
+            Bogus.Faker<SampleCustomer> customerGenerator = new Bogus.Faker<SampleCustomer>().Rules((faker, customer) =>
+            {
+                customer.Id = Guid.NewGuid().ToString();
+                customer.Name = faker.Name.FullName();
+                customer.City = faker.Person.Address.City.ToString();
+                customer.Region = faker.Person.Address.State.ToString();
+                customer.PostalCode = faker.Person.Address.ZipCode.ToString();
+                customer.MyPartitionKey = customer.Region;
+                customer.UserDefinedId = faker.Random.Int(0, 1000);
+                customer.WordsArray = faker.Random.WordsArray(1, 5);
+
+                tags.Words = faker.Random.WordsArray(1, 3);
+                tags.Numbers = faker.Random.AlphaNumeric(250);
+
+                customer.Tags = tags;
+                RecipientList.Clear();
+                int randomInt = 0;
+                customer.RecipientList = RecipientList;
+
+                randomInt = faker.Random.Int(0, 100);
+
+                numberOfAddressInfo = randomInt + numberOfAddressInfo;
+
+                int counter = 0;
+
+                while (counter < randomInt)
+                {
+                    AddressInfo addressInfo = new AddressInfo();
+
+                    addressInfo.Name = faker.Name.FullName();
+                    addressInfo.City = faker.Person.Address.City;
+                    addressInfo.Region = faker.Person.Address.State;
+                    addressInfo.PostalCode = faker.Person.Address.ZipCode;
+                    addressInfo.Quantity = faker.Random.Int(0, 1000);
+                    addressInfo.Guid = Guid.NewGuid().ToString();
+
+                    customer.RecipientList.Add(addressInfo);
+                    counter++;
+                }
+            });
+
+            return customerGenerator.Generate(number);
+        }
+
+
+    }
+    public class Tags
+    {
+        [JsonProperty(PropertyName = "words")]
+        public string[] Words { get; set; }
+        [JsonProperty(PropertyName = "numbers")]
+        public string Numbers { get; set; }
+        public Tags()
+        {
+
+        }
+    }
+
+    public class AddressInfo
+    {
+        [JsonProperty(PropertyName = "name")]
+        public string Name { get; set; }
+
+        [JsonProperty(PropertyName = "city")]
+        public string City { get; set; }
+
+        [JsonProperty(PropertyName = "postalcode")]
+        public string PostalCode { get; set; }
+
+        [JsonProperty(PropertyName = "region")]
+        public string Region { get; set; }
+
+        [JsonProperty(PropertyName = "guid")]
+        public string Guid { get; set; }
+
+        [JsonProperty(PropertyName = "quantity")]
+        public int Quantity { get; set; }
+    }
+}
